cbd7e7841b3352b88348d52c30288c7a2187474e
Keln <EgorKeln@narod.ru>
2023-09-28 09:24:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/**/
+let hours = document.getElementById('hour');
+let minute = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let ampm = document.getElementById('ampm');
+
+let hr = document.querySelector("#hr");
+let mn = document.querySelector("#mn");
+let sc = document.querySelector("#sc");
+
+setInterval(() =>{
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    
+    var am = h >= 24 ? 'PM' : 'AM';
+
+    // преобразуем 24-часовые часы в 12-часовые
+    if (h > 12){
+        h = h -12;
+    }
+
+    // добавляем ноль перед однозначным номером
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+
+    hours.innerHTML = h;
+    minute.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+    hr.style.transform = `rotateZ(${h * 30}deg)`;
+    mn.style.transform = `rotateZ(${m * 6}deg)`;
+    sc.style.transform = `rotateZ(${s * 6}deg)`;
+})
